e6b67f1b17e86ae370530100ec1acf8dc957dcd0
Joey <joeyarnstein@gmail.com>
2016-04-29 17:33:14 -0700

    initial files, pass root test

---
+++ src/test/java/WordTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class WordTest {
+  // @After
+  // public void tearDown() {
+  //   Word.clear();
+  //   //Definition.clear();
+  // }
+}
