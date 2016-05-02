e2844b28a42ee5d65d0601fa26d859f10cf38895
Joey <joeyarnstein@gmail.com>
2016-05-01 21:41:39 -0700

    id and find functionality passing tests.

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -2,11 +2,37 @@ import java.util.ArrayList;
 
 public class Word {
   private String mWordName;
-  //private static ArrayList<Word> instances;
+  private static ArrayList<Word> instances = new ArrayList<Word>();
+  private int mId;
+
 
   public Word(String wordName){
     mWordName = wordName;
-    //instances.add(this);
+    instances.add(this);
+    mId = instances.size();
+  }
 
+  public String getName(){
+    return mWordName;
+  }
+
+  public static ArrayList<Word> all(){
+    return instances;
+  }
+
+  public static void clear(){
+    instances.clear();
+  }
+
+  public int getId(){
+    return mId;
+  }
+
+  public static Word find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException e) {
+      return null;
+    }
   }
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -9,7 +9,48 @@ public class WordTest {
   // }
   @Test
   public void word_instantiatesCorrectly_true() {
-    Word testWord = new Word("Home");
+    Word testWord = new Word("Bleak");
     assertEquals(true, testWord instanceof Word);
+  }
+
+  @Test
+  public void getName_wordInstantiatesWithName_Home() {
+    Word testCategory = new Word("Bleak");
+    assertEquals("Bleak", testCategory.getName());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfWord_true() {
+    Word firstTest = new Word("Home");
+    Word secondTest = new Word("Work");
+    assertTrue(Word.all().contains(firstTest));
+    assertTrue(Word.all().contains(secondTest));
+  }
+
+  @Test
+  public void clear_emptiesAllCategoriesFromList_0() {
+    Word testWord = new Word("Home");
+    Word.clear();
+    assertEquals(Word.all().size(), 0);
+  }
+
+  @Test
+  public void getId_WordInstantiateWithAnId_1() {
+    Word.clear();
+    Word testWord = new Word("Home");
+    assertEquals(1, testWord.getId());
+  }
+
+  @Test
+  public void find_returnsWordWithSameId_secondWord() {
+    Word.clear();
+    Word firstWord = new Word("Home");
+    Word secondWord = new Word("Work");
+    assertEquals(Word.find(secondWord.getId()), secondWord);
+  }
+
+  @Test
+  public void find_returnsNullWhenNoWordFound_null() {
+    assertTrue(Word.find(999) == null);
   }
 }
